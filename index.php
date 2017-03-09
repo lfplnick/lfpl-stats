fc950bb46edbf83c48ac574a1d7ec5c10c4e6053
Nick Davenport <nick.davenport@lfpl.org>
2017-03-08 20:09:26 -0500

    Add Info and Directional buttons.
    These still need more formatting, but it's a start.

--- index.php
+++ index.php
@@ -16,25 +16,40 @@
       </div>
     </div>
 
-    <div class="container">
+    <div class="container" style="padding-bottom: 10px;">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3">
+          <h1>Info</h1>
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3 btn btn-default">
+          <h3>Easy<br/>( &lt; 2 minutes )</h3>
        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
+        <div class="col-md-3 btn btn-default">
+          <h3>Medium<br/>( 2 - 10 minutes )</h3>
+       </div>
+        <div class="col-md-3 btn btn-default">
+          <h3>Hard<br/>( &gt;10 minutes )</h3>
+       </div>
       </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h1>Directional</h1>
+        </div>
+        <div class="col-md-3 btn btn-default">
+          <h3>Easy<br/>( &lt; 2 minutes )</h3>
+       </div>
+        <div class="col-md-3 btn btn-default">
+          <h3>Medium<br/>( 2 - 10 minutes )</h3>
+       </div>
+        <div class="col-md-3 btn btn-default">
+          <h3>Hard<br/>( &gt;10 minutes )</h3>
+       </div>
+      </div>
+
 
       <hr>
 
