ece2004131c1ea03fd771f13c360add734e4eb32
Nick Davenport <nick.davenport@lfpl.org>
2017-03-17 14:41:34 -0400

    Add SQL for outreach tables.

--- includes/tables.sql
+++ includes/tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE stat_branches(
     branches_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
     branches_name varchar(255) NOT NULL,
     branches_abbr varchar(255)
-);
+)ENGINE=InnoDB;
 
 CREATE TABLE stat_desks(
     desks_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -32,3 +32,59 @@ CREATE TABLE stat_daily_stat_types(
     dst_name varchar(255) NOT NULL,
     dst_desc varchar(255)
 );
+
+CREATE TABLE stat_outreach_primary_type(
+    opt_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    opt_name varchar(255) NOT NULL,
+    opt_enabled boolean NOT NULL
+)ENGINE=InnoDB;
+
+
+CREATE TABLE stat_outreach_secondary_type(
+    ost_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ost_name varchar(255) NOT NULL,
+    opt_id int unsigned NOT NULL,
+    ost_enabled boolean NOT NULL,
+    FOREIGN KEY fk_primary_type(opt_id)
+     REFERENCES stat_outreach_primary_type(opt_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
+
+CREATE TABLE stat_outreach_stats(
+    os_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    branches_id int unsigned NOT NULL,
+
+    --
+    -- Primary Type ID
+    --
+    -- Foreign key to stat_outreach_primary_type.opt_id. Should not actually
+    -- allow this to be NULL, but if primary type is deleted it is set to null
+    -- rather than deleting record.
+    --
+    opt_id int unsigned,
+
+    --
+    -- Secondary Type ID
+    --
+    -- Foreign key to stat_outreach_secondary_type.ost_id. This one can be NULL.
+    --
+    ost_id int unsigned,
+    os_datetime DATETIME NOT NULL,
+    os_attendance int unsigned NOT NULL,
+    os_name varchar(255) NOT NULL,
+    FOREIGN KEY fk_branch(branches_id)
+     REFERENCES stat_branches(branches_id)
+     ON DELETE CASCADE
+     ON UPDATE CASCADE,
+    FOREIGN KEY fk_primary_type(opt_id)
+     REFERENCES stat_outreach_primary_type(opt_id)
+     ON DELETE SET NULL
+     ON UPDATE CASCADE,
+    FOREIGN KEY fk_secondary_type(ost_id)
+     REFERENCES stat_outreach_secondary_type(ost_id)
+     ON DELETE SET NULL
+     ON UPDATE CASCADE
+)ENGINE=InnoDB;
+
