ffb0a5ea60912d5c14d283254a973c9cc571ad4a
Nick Davenport <nick.davenport@lfpl.org>
2017-03-28 21:12:01 -0400

    Parse request method in StatsHandler.

--- api/v1/StatsHandler.php
+++ api/v1/StatsHandler.php
@@ -3,13 +3,16 @@
 class StatsHandler {
     private $whatitdo;
     private $resource; // i.e. daily stats, outreach stats, program stats
+    private $method;   // request method
     public function __construct( $whatitdo ){
         $this->whatitdo = $whatitdo;
         $this->resource = $this->parseResource();
+        $this->method = $this->parseMethod();
     }
 
     public function handle(){
-        var_dump( $this->resource );
+        echo "Resource: {$this->resource}<br>";
+        echo "Method: {$this->method}<br>";
     }
 
     private function parseResource(){
@@ -31,5 +34,25 @@ class StatsHandler {
         endswitch;
 
         return $resource;
+    }
+
+    private function parseMethod(){
+        $method = '';
+        switch( strtolower( $_SERVER['REQUEST_METHOD'] ) ):
+            case 'get':
+                $method = 'get';
+                break;
+            case 'post':
+                $method = 'post';
+                break;
+            case 'put':
+                $method = 'put';
+                break;
+            case 'delete':
+                $method = 'delete';
+                break;
+        endswitch;
+
+        return $method;
     }
 }
