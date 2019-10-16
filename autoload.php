a01b10d2a13904cfd73ed491ed25a02975540013
Nick Davenport <nick.davenport@lfpl.org>
2019-10-16 09:08:00 -0400

    Add calls to array() rather than using shorthand.
    Shorthand notation for arrays is not supported in PHP 5.3.

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 global $sgClasses;
 
-$sgClasses = [
+$sgClasses = array(
     'AutoLoader' => __DIR__ . '/includes/AutoLoader.php',
     'Branch' => __DIR__ . '/includes/Branch.php',
     'BranchesHandler' => __DIR__ . '/api/v1/BranchesHandler.php',
@@ -11,4 +11,4 @@ $sgClasses = [
     'InitializeDatabase' => __DIR__ . '/includes/InitializeDatabase.php',
     'RequestHandler' => __DIR__ . '/api/v1/RequestHandler.php',
     'StatsHandler' => __DIR__ . '/api/v1/StatsHandler.php'
-];
+);
